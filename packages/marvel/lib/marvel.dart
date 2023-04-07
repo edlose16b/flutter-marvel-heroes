d1df29c0f1c045a34968469b6f8b593de3209c9b
Edlose <edwin.cbalbin@gmail.com>
2023-04-07 00:02:34 -0500

    fix(marvel): :ambulance: adding export to marvel library

--- packages/marvel/lib/marvel.dart
+++ packages/marvel/lib/marvel.dart
@@ -1,2 +1,18 @@
 /// A Very Good Project created by Very Good CLI.
 library marvel;
+
+export 'package:marvel/src/core/failures.dart';
+export 'package:marvel/src/domain/entities/character.dart';
+export 'package:marvel/src/domain/entities/comic.dart';
+export 'package:marvel/src/domain/entities/event.dart';
+export 'package:marvel/src/domain/entities/serie.dart';
+export 'package:marvel/src/domain/entities/story.dart';
+export 'package:marvel/src/domain/repositories/characters_repository.dart';
+export 'package:marvel/src/infraestructure/datasources/characters_remote_datasource.dart';
+export 'package:marvel/src/infraestructure/datasources/remote_datasource.dart';
+export 'package:marvel/src/infraestructure/dto/marvel_response.dart';
+export 'package:marvel/src/infraestructure/dto/pagination_response.dart';
+export 'package:marvel/src/infraestructure/repositories/characters_repository_impl.dart';
+export 'package:marvel/src/infraestructure/repositories/sample_characters_repository.dart';
+
+export 'src/mock/mocks.dart';
